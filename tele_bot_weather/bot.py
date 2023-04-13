bc36d92c4c28080f2f4d919b27a6fab8500403e2
Chingiz Nasibbeyli <reis_969@mail.ru>
2023-04-13 16:51:55 +0300

    добрый вечер

--- tele_bot_weather/bot.py
+++ tele_bot_weather/bot.py
@@ -1,7 +1,8 @@
-from api.api_bot_site import dp, weather_token
-from aiogram import types, executor
-from module import func_weat
-
+from api.api_bot_site import bot
+from aiogram import types
+from module import func_weather, lat, lon
+import requests
+from api.api_bot_site import weather_token
 
 
 def func_keyboard():
@@ -15,33 +16,44 @@ def func_keyboard():
     return markup
 
 
-@dp.message_handler(commands=['start', 'help'])
-async def start_bot(message):
-    await message.reply(f'Привет {message.from_user.first_name}.\nЯ бот Который узнает погоду во всем мире.'
-                        f'\nЧтобы узнать погоду на сегодня нажми "Узнать погоду в моем городе⛅"',
-                        reply_markup=func_keyboard())
+@bot.message_handler(commands=['start', 'help'])
+def start_bot(message):
+    bot.send_message(message.from_user.id, f'Привет {message.from_user.first_name}.'
+                                           f'\nЯ бот Который узнает погоду во всем мире.'
+                                           f'\nЧтобы узнать погоду на сегодня нажмите "Узнать погоду в моем городе⛅"',
+                     reply_markup=[func_keyboard()])
 
 
-response = func_weat()
-
-
-@dp.message_handler(content_types='text')
-async def commune(message: types.Message):
+@bot.message_handler(content_types='text')
+def commune(message: types.Message):
     if message.text == 'Поздороваться👋':
-        await message.reply(f'Привет {message.from_user.first_name}')
+        bot.send_message(message.from_user.id, f'Привет {message.from_user.first_name}', reply_markup=[func_keyboard()])
 
     elif message.text == 'Как тебя Зовут?🤔':
-        await message.reply('Меня зовут Чаппи')
+        bot.send_message(message.from_user.id, 'Меня зовут Чаппи')
 
     elif message.text == 'Узнать погоду в моем городе⛅':
-        await message.reply('weather in your city: ', response)
+        api_weather = requests.get(
+            f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={weather_token}&units=metric'
+        )
+        data = api_weather.json()
+        temp = data['main']['temp']
+        bot.send_message(message.from_user.id,  f'Погода в товем городе {temp}',
+                         reply_markup=[func_keyboard()])
 
     elif message.text == 'Узнать погоду в другом городе⛅':
-        pass
+        bot.send_message(message.from_user.id, 'В каком городе хотели узнать погоду⛅:',
+                         reply_markup=[func_keyboard()])
+        bot.register_next_step_handler(message, weather)
 
     elif message.text == 'Задать вопрос❓':
         pass
 
 
-if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+def weather(message):
+    text = message.text
+    w = func_weather(text)
+    bot.send_message(message.from_user.id, f'{w}')
+
+
+bot.polling(none_stop=True, interval=0)
--- tele_bot_weather/module.py
+++ tele_bot_weather/module.py
@@ -2,6 +2,8 @@ from api.api_bot_site import weather_token
 import requests
 from urllib.request import urlopen
 import json
+from api.api_bot_site import bot
+
 
 def _get_ip_data() -> dict:
     url = 'http://ipinfo.io/json'
@@ -14,29 +16,18 @@ lat = data['loc'].split(',')[0]
 lon = data['loc'].split(',')[1]
 
 
-def func_weather(city, weather_token):
+def func_weather(city):
     r = requests.get(
-        f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={weather_token}'
+        f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={weather_token}&units=metric'
     )
     data = r.json()
-    print(data)
-
-
-def func_weat():
-    a = requests.get(
-        f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={weather_token}'
-    )
-    data = a.json()
-    name = data['name']
-    temp = data['main']['temp']
-    print(f'{name}\n{temp}')
-    return
-
-
-# def weather():
-#     result = func_weat()
-#     print(func_weat())
+    try:
+        name = data['name']
+        temp = data['main']['temp']
+        return f'Погода в {name} {temp} C'
+    except:
+        return 'такого города нет'
 
 
 if __name__ == '__main__':
-    func_weat()
+    func_weather()
